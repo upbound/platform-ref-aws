55fa8c6ec7ae3d9f382451e484642facd7a8524b
Yury Tsarev <yury@upbound.io>
2023-04-26 00:18:17 +0200

    Fix uptest deletion stuck
    The uptest run was stuck indefinitely while waiting for Release cleanup
    due to Release recreation by associated XServices XR right after the
    original cleanup
    
    * Switch to the proper pre-delete-hook
    * Stop XService reconciliation in the hook to avoid Release recreation after
      hook-based cleanup
    
    Signed-off-by: Yury Tsarev <yury@upbound.io>

--- examples/testhooks/delete-release.sh
+++ examples/testhooks/delete-release.sh
@@ -3,4 +3,7 @@ set -aeuo pipefail
 
 # Delete the release before deleting the cluster not to orphan the release object
 # Note(turkenh): This is a workaround for the infamous dependency problem during deletion.
+# Note(ytsarev): In addition to helm Release deletion we also need to pause
+# XService reconciler to prevent it from recreating the Release.
+${KUBECTL} annotate xservices.aws.platformref.upbound.io --all crossplane.io/paused="true"
 ${KUBECTL} delete release --all
