7ed96f4bb648dee47cd37ef36b2a4cd802972d24
Yury Tsarev <yury@upbound.io>
2023-06-27 12:43:13 +0200

    Add delete hook for App
    Signed-off-by: Yury Tsarev <yury@upbound.io>

---
+++ examples/testhooks/delete-release-app.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -aeuo pipefail
+
+# Delete the release before deleting the cluster not to orphan the release object
+# Note(turkenh): This is a workaround for the infamous dependency problem during deletion.
+# Note(ytsarev): In addition to helm Release deletion we also need to pause
+# XService reconciler to prevent it from recreating the Release.
+${KUBECTL} annotate --overwrite xapps.aws.platformref.upbound.io --all crossplane.io/paused="true"
+${KUBECTL} delete release -l crossplane.io/claim-name=platform-ref-aws-ghost
