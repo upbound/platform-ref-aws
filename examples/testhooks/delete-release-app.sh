22027d7554f70f069e8a6cd4bad1bd2cb69a599a
Christopher Haar <chhaar30@googlemail.com>
2023-07-19 22:05:37 +0200

    feat(irsa): implement irsa composition
    Signed-off-by: Christopher Haar <chhaar30@googlemail.com>

--- examples/testhooks/delete-release-app.sh
+++ examples/testhooks/delete-release-app.sh
@@ -6,6 +6,4 @@ set -aeuo pipefail
 # Note(ytsarev): In addition to helm Release deletion we also need to pause
 # XService reconciler to prevent it from recreating the Release.
 ${KUBECTL} annotate xapps.aws.platformref.upbound.io --all crossplane.io/paused="true"
-app=$(${KUBECTL} get releases --no-headers -o custom-columns="NAME:metadata.name" | grep "platform-ref-aws-ghost")
-${KUBECTL} patch release $app -p '{"metadata":{"finalizers":null}}' --type=merge
 ${KUBECTL} delete release -l crossplane.io/claim-name=platform-ref-aws-ghost
